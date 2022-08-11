9734ee3a4e491369143dde082ca56b536ce07633
terwer <youweics@163.com>
2022-08-11 10:21:10 +0800

    :art: 备份本地笔记

--- pushGithub.ps1
+++ pushGithub.ps1
@@ -1,9 +1,10 @@
 $msg=':art: 备份本地笔记'
+$srcgithubUrl=git@github.com:terwer/my-note-docker.git
 
 git config user.name "terwer"
 git config user.email "youweics@163.com"
 git add -A
 git commit -m "${msg}"
 # 推送到github main分支
-git push git@github.com:terwer/my-note-docker.git main:main
+git push $srcgithubUrl main:main
 Write-Output "Github备份完毕."
